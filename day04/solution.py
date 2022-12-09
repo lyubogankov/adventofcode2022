fd2b6f42a47026b44bc7e96aa2160236d2cf75ba
lyubogankov <lyubogankov13@gmail.com.com>
2022-12-08 19:55:02 -0800

    rearranging comments in solution.py

--- day04/solution.py
+++ day04/solution.py
@@ -1,11 +1,5 @@
 import re
 
-
-# Structure of input data
-# a-b,c-d
-#   where each pair of numbers separated by hyphens indicates the range of ID numbers (a (inclusive) to b (inclusive))
-#   and two ID ranges are given per line, since the elves are paired up.
-
 def part_one(raw_data):
     # Question: In how many assignment pairs does one range fully contain the other?
     #   To answer this, we can use set properties!
@@ -24,20 +18,28 @@ def part_two(raw_data):
     print(f"Part 2: number of elf pairs with any intersection of responsibilities: {intersection_counter}")
 
 if __name__ == '__main__':
-    # raw_data structure:
-    #   - list of lists
-    #   - each list represents the pair of elves (and therefore has two elements)
-    #   - the two elements are sets of ids for which the elves are responsible
-    parser = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+    
     for inputfile in ['example.txt', 'input.txt']:
         print(f'--- {inputfile}')
+
+        # Structure of input data
+        # a-b,c-d
+        #   where each pair of numbers separated by hyphens indicates the range of ID numbers (a (inclusive) to b (inclusive))
+        #   and two ID ranges are given per line, since the elves are paired up.
         raw_data = []
+        parser = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
         with open(inputfile, 'r') as _inputfile:
             for line in _inputfile.readlines():
                 elf1_start, elf1_end, elf2_start, elf2_end = map(int, parser.search(line).groups())
+
+                # raw_data structure:
+                #   - list of lists
+                #   - each list represents the pair of elves (and therefore has two elements)
+                #   - the two elements are sets of ids for which the elves are responsible
                 raw_data.append([
                     {id for id in range(elf1_start, elf1_end + 1)},
                     {id for id in range(elf2_start, elf2_end + 1)}
                 ])
+                
         part_one(raw_data)
         part_two(raw_data)
