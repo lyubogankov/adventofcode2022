51242ed8f54c9a24daca40fe2f5f4525f7614340
lyubogankov <lyubogankov13@gmail.com.com>
2022-12-17 00:10:14 -0800

    day09: animated part one example in terminal (input grid too large, need to figure out alternative)

--- day09/solution.py
+++ day09/solution.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from pprint import pprint
-import hashlib
+import os
+import time
 
 # colors
 RED = '\033[31m'
@@ -94,9 +95,11 @@ def read_input_file_into_move_list(inputfile):
 ### printing
 
 # generic printer
-def generate_print_grid_string(items, grid):
+def generate_print_grid_string(items, grid, animationmode=False):
     '''Creates string to be printed from list of items, note that items are applied FIFO (so they can override each other).
     Wrote separately from print function so I can do unit tests against this function.
+
+    Animationmode = don't print as many dots, only do it for the border.
     '''
 
     # sort the items by x (row), then y (col) -- reverse order of loops below (y (row) = outer, x (col) = inner)
@@ -111,6 +114,8 @@ def generate_print_grid_string(items, grid):
     for row_idx in reversed(range(grid.botr.y, grid.topl.y+1)):  # need to reverse bc row 0 is on bottom, n on top...
         for col_idx in range(grid.topl.x, grid.botr.x+1):
             nextchar = '.'
+            if animationmode and grid.topl.x < col_idx < grid.botr.x and grid.botr.y < row_idx < grid.topl.y:
+                nextchar = ' '
             while curr_item < len(items) and items[curr_item].point.x == col_idx and items[curr_item].point.y == row_idx:
                 nextchar = items[curr_item].printchar
                 curr_item += 1
@@ -120,13 +125,15 @@ def generate_print_grid_string(items, grid):
     return print_grid_string
 
 # general printer, for showing changes to grid state
-def generate_current_grid_state_string(h_point, t_point, grid, start_point):
+def generate_current_grid_state_string(h_point, t_point, grid, start_point, animationmode=False):
+    startpt_printchar = f'{RED}s{RESET}' if animationmode else 's'
     return  generate_print_grid_string(
-                items=[PrintItem('s', start_point), PrintItem('T', t_point), PrintItem('H', h_point)],
-                grid=grid
+                items=[PrintItem(startpt_printchar, start_point), PrintItem('T', t_point), PrintItem('H', h_point)],
+                grid=grid,
+                animationmode=animationmode
             )
-def print_current_grid_state(h_point, t_point, grid, start_point):
-    print(generate_current_grid_state_string(h_point, t_point, grid, start_point), end='\n\n')
+def print_current_grid_state(*args, **kwargs):
+    print(generate_current_grid_state_string(*args, **kwargs), end='\n\n')
 
 # printer for part one
 def generate_unique_t_locations_string(t_locations_set, grid, start_point, color_s=False):
@@ -135,8 +142,8 @@ def generate_unique_t_locations_string(t_locations_set, grid, start_point, color
                 items=[PrintItem('#', point) for point in t_locations_set] + [PrintItem(startpt_printchar, start_point)],
                 grid=grid
             )
-def print_unique_t_locations(t_locations_set, grid, start_point):
-    print(generate_unique_t_locations_string(t_locations_set, grid, start_point), end='\n\n')
+def print_unique_t_locations(*args, **kwargs):
+    print(generate_unique_t_locations_string(*args, **kwargs), end='\n\n')
 
 ### rope simulation
 def update_h_and_t_pos(h_initial, t_initial, h_atomic_move):
@@ -166,7 +173,7 @@ def update_dynamic_grid(h, grid):
 
 def simulate_rope_partone(
         move_list, fixed_grid=None, start_point=Point_2D(x=0, y=0),
-        _print=False, print_atomic_moves=False, color_start_point=True):
+        _print=False, print_atomic_moves=False, color_start_point=True, animationmode=False):
     '''Start point will be the bottom left corner of the grid.'''
 
     # initial condition
@@ -189,7 +196,7 @@ def simulate_rope_partone(
 
     if _print:
         print('Initial state:')
-        print_current_grid_state(h, t, grid, start_point)
+        print_current_grid_state(h, t, grid, start_point, animationmode)
     for move in move_list:
         if _print:
             print(f'== {move.name} {move.repetitions} ==')
@@ -199,7 +206,10 @@ def simulate_rope_partone(
                 grid = update_dynamic_grid(h, grid)
             t_move_set.add(t)
             if _print and print_atomic_moves:
-                print_current_grid_state(h, t, grid, start_point)
+                if animationmode:
+                    os.system('clear') # linux only :/
+                    print_current_grid_state(h, t, grid, start_point, animationmode=True)
+                    time.sleep(0.1)
         if _print and not print_atomic_moves:
             print_current_grid_state(h, t, grid, start_point)
 
@@ -207,27 +217,51 @@ def simulate_rope_partone(
 
 if __name__ == '__main__':
 
-    fixed_grid = Grid(
+    fixed_grid_example = Grid(
         topl=Point_2D(x=0, y=4),
         botr=Point_2D(x=5, y=0)
     )
 
-    for inputfile in ['example.txt', 'input.txt']:
+    fixed_grid_input = Grid(
+        topl=Point_2D(x=0, y=267),
+        botr=Point_2D(x=363, y=0)
+    )
+
+    # # solving parts 1 / 2
+    # for inputfile in ['example.txt', 'input.txt']:
+    #     example = inputfile == 'example.txt'
+
+    #     move_list = read_input_file_into_move_list(inputfile)
+    #     t_move_set, t_move_str = simulate_rope_partone(
+    #         move_list,
+    #         fixed_grid=fixed_grid_example if example else None,
+    #         _print=example,
+    #         print_atomic_moves=example,
+    #         color_start_point=True
+    #     )
+    #     print(f'Number of unique spots T has visited: {len(t_move_set)}')
+    #     print(t_move_str, end='\n\n')
+
+    # animation
+    '''TODO
+    create GIF out of individual frames for example / input.txt
+
+    especially input, bc it's too large to render properly in terminal.
+    https://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python
+    '''
+    for inputfile in ['example.txt']: #, 'input.txt']:  # input is too large for the terminal animation.  perhaps create GIF?
         example = inputfile == 'example.txt'
 
         move_list = read_input_file_into_move_list(inputfile)
         t_move_set, t_move_str = simulate_rope_partone(
             move_list,
-            fixed_grid=fixed_grid if example else None,
-            _print=example,
-            print_atomic_moves=example,
-            color_start_point=True
+            fixed_grid=fixed_grid_example if example else fixed_grid_input,
+            start_point=Point_2D(0, 0) if example else Point_2D(299, 220),
+            _print=True,
+            print_atomic_moves=True,
+            color_start_point=True,
+            animationmode=True
         )
-        print(f'Number of unique spots T has visited: {len(t_move_set)}')
-        print(t_move_str, end='\n\n')
-        
 
-'''
-TODO
-4. create unit tests for generate_print_str based on example.txt
-'''
+        # print('.'*364)
+        # _ = input('change your resolution.  ENTER when done.')
