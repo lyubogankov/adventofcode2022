bce7ff0213bfa118dd53f6e03e1cbe07df58e8cc
lyubogankov <lyubogankov13@gmail.com.com>
2022-12-22 09:15:55 -0800

    completed day10 part one -- solution + unit tests

--- day10/solution.py
+++ day10/solution.py
@@ -1,4 +1,4 @@
-# from collections import namedtuple
+from collections import namedtuple
 
 ### input file parsing
 class Instruction:
@@ -18,6 +18,8 @@ class Instruction:
     def __repr__(self):
         return f"Instruction(name='{self.name}', arg={self.arg}, num_cycles={self.num_cycles}, operation={self.operation_fn})"
 
+    def prettyprint(self, argwidth, namewidth=4):
+        return eval(f"f'{self.name:<{namewidth}} {str(self.arg):>{argwidth}}' if self.arg else f'{self.name:<{namewidth+1+argwidth}}'")
 
 def parse_instruction_list_from_file(instruction_file):
     with open(instruction_file, 'r') as instfile:
@@ -33,42 +35,68 @@ def parse_instruction_list_from_file(instruction_file):
     return instruction_list
 
 ### manipulation / simulation / game loop
-def run_instructions_and_return_cpustate(instruction_list, start_cycle=0, end_cycle=None, mid_instruction=0, cpu_state={'register_x' : 1}, _print=False):
+def run_instructions_and_return_cpustate(instr_list, instr_start_idx=0, start_cycle=0, end_cycle=None, mid_instr_start_cycles=0, cpu_state={'register_x' : 1}, _print=False):
 
+    if _print:
+        print(f'run_instructions_and_return_cpustate(start_cycle={start_cycle}, end_cycle={end_cycle}, mid_instr_start_cycles={mid_instr_start_cycles}), instr_start_idx={instr_start_idx})')
+
+    if start_cycle == end_cycle or (type(end_cycle) == int and start_cycle >= end_cycle):
+        return cpu_state, 0, None
     cycle_count = start_cycle
-    for instruction in instruction_list:
+
+    for instr_num, instruction in enumerate(instr_list[instr_start_idx:], start=1):
         if _print:
-            print(f'cpu state: {cpu_state}')
-            print(f'\t[cycle {cycle_count:>3}] {str(instruction)}')
+            print(f'\t[{str(cycle_count):>3}]   {(instruction.prettyprint(argwidth=3))}  |  cpu state before instr: {cpu_state}')
         # instructions have different execution times, measured in number of cycles
-        for instr_cycle in range(mid_instruction, instruction.num_cycles):
-            # test for simulation break
-            if cycle_count == end_cycle:
-                return cpu_state, instr_cycle
+        for instr_cycle in range(mid_instr_start_cycles if instr_num == 1 else 0, instruction.num_cycles):
             cycle_count += 1
+            if cycle_count == end_cycle:
+                return cpu_state, instr_num, instr_cycle+1
         # apply operation, if needed
         if instruction.operation:
             cpu_state['register_x'] = instruction.operation(instruction.arg, cpu_state['register_x'])
     # if we weren't given an end cycle, we'll run out of instructions.  still want to return cpu state!
-    return cpu_state, None
+    return cpu_state, instr_num, None
 
 def run_instructions_on_cpu_partone(instr_list, _print=False):
 
+    RegisterSnapshot = namedtuple('RegisterSnapshot', ['cycle', 'val'])
+
     cpu_state = {'register_x' : 1}
+    snapshots_register_x = []
 
-    # mid_instr_break = 0
-    # start, end, width = 20, 220, 40
-    # for start_cycle, end_cycle, instr_slice in [(0, 20, slice(20))] + \
-    #         [(s, e, slice(s, e)) for s, e in zip(range(start, end+1, width), range(start+width, end+width+1, width))]:
-    #     instructions = instr_list[instr_slice]
-    #     cpu_state, cycles_into_instr = run_instructions_and_return_cpustate(instructions, start_cycle, end_cycle, cycles_into_instr, cpu_state, _print)
-    #     if _print:
-    #         print(f'cpu state: {cpu_state}')
-    #     print(cpu_state)
+    # [0, 20)
+    instrs_completed = 0
+    cpu_state, instructions_run, mid_instr_cycle_count = run_instructions_and_return_cpustate(instr_list, end_cycle=20, cpu_state=cpu_state, _print=_print)
+    instrs_completed += instructions_run
+    snapshots_register_x.append(RegisterSnapshot(cycle=20, val=cpu_state['register_x']))
 
-    # I don't like this approach - not handling if I return mid-cycle.  If I do that, when I restart sim, need to pass in that instruction as well.
+    # [[20, 60), [180, 220)]
+    for offset in range(0, 181, 40):
+        if mid_instr_cycle_count > 0:
+            instrs_completed -= 1
+        cpu_state, instructions_run, mid_instr_cycle_count = \
+                run_instructions_and_return_cpustate(instr_list, instr_start_idx=instrs_completed, mid_instr_start_cycles=mid_instr_cycle_count,
+                                                    start_cycle=20+offset, end_cycle=60+offset, cpu_state=cpu_state, _print=_print)
+        instrs_completed += instructions_run
+        snapshots_register_x.append(RegisterSnapshot(cycle=60+offset, val=cpu_state['register_x']))
+    
+    return snapshots_register_x
 
+def calculate_signal_strength_sum(instr_list, _print=False):
+    snapshots_register_x = run_instructions_on_cpu_partone(instr_list, _print)
+    return sum(snapshot.cycle * snapshot.val for snapshot in snapshots_register_x)
 
 if __name__ == '__main__':
-    instr_list = parse_instruction_list_from_file('example_two.txt')
-    run_instructions_on_cpu_partone(instr_list)
+    instr_list = parse_instruction_list_from_file('input.txt')
+    print(f'Part one: {calculate_signal_strength_sum(instr_list, _print=True)}')
+
+
+'''
+Potential switchups that might occur for part two:
+- more than one register, instructions affect different registers based on some rule
+- additional instruction type(s)
+- more than one CPU, instructions get mapped to different CPUs based on some rule
+
+Tried to parameterize the sim funciton to be able to address these if they come up!
+'''
--- day10/test_solution.py
+++ day10/test_solution.py
@@ -13,7 +13,6 @@ class TestDay10(unittest.TestCase):
         '''Testing whether we can correctly parse input file into Instruction objects.
         Instruction object's __str__ method outputs same format as input file.
         '''
-
         for examplefile in [self.EXAMPLEONE, self.EXAMPLETWO]:
             with self.subTest(i=examplefile):
                 instr_list = solution.parse_instruction_list_from_file(examplefile)
@@ -25,6 +24,19 @@ class TestDay10(unittest.TestCase):
 
                 for instr, line in zip(instr_list, lines):
                     self.assertEqual(str(instr), line)
+
+    def test_partone_register_snapshots(self):
+        '''Day10 part one: simulating instructions on a CPU.
+        At specific cycle counts, need to sample the CPU's X register value.  Testing that here!'''
+        instr_list = solution.parse_instruction_list_from_file(self.EXAMPLETWO)
+        sampled_register_values = [21, 19, 18, 21, 16, 18]
+        for sim_output, answer in zip(solution.run_instructions_on_cpu_partone(instr_list), sampled_register_values):
+            with self.subTest(i=f'cycle {sim_output.cycle}'):
+                self.assertEqual(sim_output.val, answer)
+
+    def test_partone_signal_strength_sum(self):
+        instr_list = solution.parse_instruction_list_from_file(self.EXAMPLETWO)
+        self.assertEqual(solution.calculate_signal_strength_sum(instr_list), 13140)
 
 if __name__ == '__main__':
     unittest.main()
