8ceae490c8f45472a99decf89d0bc4b12905faec
lyubogankov <lyubogankov13@gmail.com.com>
2022-12-23 10:55:23 -0800

    Day10 complete, with solution + unit tests :)

--- day10/solution.py
+++ day10/solution.py
@@ -35,13 +35,16 @@ def parse_instruction_list_from_file(instruction_file):
     return instruction_list
 
 ### manipulation / simulation / game loop
-def run_instructions_and_return_cpustate(instr_list, instr_start_idx=0, start_cycle=0, end_cycle=None, mid_instr_start_cycles=0, cpu_state={'register_x' : 1}, _print=False):
+def run_instructions_and_return_cpustate(instr_list, instr_start_idx=0, start_cycle=0, num_cycles=None, mid_instr_start_cycles=0, cpu_state={'register_x' : 1}, _print=False):
+
+    if num_cycles is not None:
+        end_cycle = start_cycle + num_cycles
 
     if _print:
-        print(f'run_instructions_and_return_cpustate(start_cycle={start_cycle}, end_cycle={end_cycle}, mid_instr_start_cycles={mid_instr_start_cycles}), instr_start_idx={instr_start_idx})')
+        print(f'run_instructions_and_return_cpustate(start_cycle={start_cycle}, num_cycles={num_cycles}, mid_instr_start_cycles={mid_instr_start_cycles}), instr_start_idx={instr_start_idx}) | end_cycle={end_cycle}')
 
-    if start_cycle == end_cycle or (type(end_cycle) == int and start_cycle >= end_cycle):
-        return cpu_state, 0, None
+    # if start_cycle == end_cycle or (type(end_cycle) == int and start_cycle >= end_cycle):
+    #     return cpu_state, 0, None
     cycle_count = start_cycle
 
     for instr_num, instruction in enumerate(instr_list[instr_start_idx:], start=1):
@@ -55,19 +58,24 @@ def run_instructions_and_return_cpustate(instr_list, instr_start_idx=0, start_cy
         # apply operation, if needed
         if instruction.operation:
             cpu_state['register_x'] = instruction.operation(instruction.arg, cpu_state['register_x'])
+        # Added clause -- if we are trying to simulate clock boundary between cycles (num_cycles=0),
+        #                 need to end after instruction is applied.
+        if cycle_count == end_cycle:
+            return cpu_state, instr_num, 0
     # if we weren't given an end cycle, we'll run out of instructions.  still want to return cpu state!
     return cpu_state, instr_num, None
 
-def run_instructions_on_cpu_partone(instr_list, _print=False):
+### part one
+RegisterSnapshot = namedtuple('RegisterSnapshot', ['cycle', 'val'])
 
-    RegisterSnapshot = namedtuple('RegisterSnapshot', ['cycle', 'val'])
+def run_instructions_on_cpu_partone(instr_list, _print=False):
 
     cpu_state = {'register_x' : 1}
     snapshots_register_x = []
 
     # [0, 20)
     instrs_completed = 0
-    cpu_state, instructions_run, mid_instr_cycle_count = run_instructions_and_return_cpustate(instr_list, end_cycle=20, cpu_state=cpu_state, _print=_print)
+    cpu_state, instructions_run, mid_instr_cycle_count = run_instructions_and_return_cpustate(instr_list, num_cycles=20, cpu_state=cpu_state, _print=_print)
     instrs_completed += instructions_run
     snapshots_register_x.append(RegisterSnapshot(cycle=20, val=cpu_state['register_x']))
 
@@ -77,7 +85,7 @@ def run_instructions_on_cpu_partone(instr_list, _print=False):
             instrs_completed -= 1
         cpu_state, instructions_run, mid_instr_cycle_count = \
                 run_instructions_and_return_cpustate(instr_list, instr_start_idx=instrs_completed, mid_instr_start_cycles=mid_instr_cycle_count,
-                                                    start_cycle=20+offset, end_cycle=60+offset, cpu_state=cpu_state, _print=_print)
+                                                     start_cycle=20+offset, num_cycles=40, cpu_state=cpu_state, _print=_print)
         instrs_completed += instructions_run
         snapshots_register_x.append(RegisterSnapshot(cycle=60+offset, val=cpu_state['register_x']))
     
@@ -87,10 +95,84 @@ def calculate_signal_strength_sum(instr_list, _print=False):
     snapshots_register_x = run_instructions_on_cpu_partone(instr_list, _print)
     return sum(snapshot.cycle * snapshot.val for snapshot in snapshots_register_x)
 
+### part two
+def generate_sprite_pos_str(middle_pixel_pos, linewidth=40):
+    # ensure we don't print off screen
+    sprite_start_pixel = max(0,           middle_pixel_pos - 1)
+    sprite_end_pixel   = min(linewidth-1, middle_pixel_pos + 1)
+    sprite_width = (sprite_end_pixel - sprite_start_pixel) + 1
+    return '.'*sprite_start_pixel + '#'*sprite_width + '.'*(linewidth - sprite_width - sprite_start_pixel)
+
+def generate_crt_printstr_parttwo(instr_list, cycle_num_start=1, cycle_num_end=240, _print=False, gen_test_str=False):
+    '''
+    - Initial sprite pos = 1 (register x value), and regX value represents the middle pixel (sprite is 3 wide)
+    - CRT rows go from [0, 39] or [0, 40) - whichever notation you prefer (they are 40 pixels wide)
+    - CRT prints from top left to bottom right, one pixel DURING each cycle
+    '''
+
+    CRT_LINEWIDTH = 40
+    crt_str = ''
+
+    cpu_state = {'register_x' : 1}
+    current_instr = 0
+    mid_instr_cycle_count = 0
+
+    if gen_test_str:
+        test_str = f"Sprite position: {generate_sprite_pos_str(cpu_state['register_x'])}\n\n"
+
+    for cycle_num in range(cycle_num_start, cycle_num_end + 1):
+        
+        ## cycle start
+        if gen_test_str and mid_instr_cycle_count == 0:
+            test_str += f'Start cycle  {cycle_num:>2}: begin executing {str(instr_list[current_instr])}\n'
+        
+        ## during cycle, generate CRT string
+        crt_pixel_pos = (cycle_num-1) % CRT_LINEWIDTH
+        # if we're at the start of a newline, need to terminate old one!
+        if crt_pixel_pos == 0 and crt_str != '':
+            crt_str += '\n' 
+        crt_str += '#' if (cpu_state['register_x'] - 1 <= crt_pixel_pos <= cpu_state['register_x'] + 1) else '.'
+        if gen_test_str:
+            test_str += f'During cycle {cycle_num:>2}: CRT draws pixel in position {crt_pixel_pos}\n'
+            test_str += f'Current CRT row: {crt_str}\n'
+
+        ## after cycle, apply instruction to CPU
+        cpu_state, instructions_run, mid_instr_cycle_count = \
+                run_instructions_and_return_cpustate(instr_list, instr_start_idx=current_instr,
+                                                     mid_instr_start_cycles=mid_instr_cycle_count,
+                                                     start_cycle=cycle_num-1, num_cycles=1, cpu_state=cpu_state, _print=_print)
+        current_instr += instructions_run
+        if mid_instr_cycle_count > 0:
+            current_instr -= 1
+        
+        # finish out instruction, if needed
+        if mid_instr_cycle_count == instr_list[current_instr].num_cycles:
+            prior_cpu_register_x_val = cpu_state['register_x']
+            cpu_state, instructions_run, mid_instr_cycle_count = \
+                    run_instructions_and_return_cpustate(instr_list, instr_start_idx=current_instr,
+                                                        mid_instr_start_cycles=mid_instr_cycle_count,
+                                                        start_cycle=cycle_num, num_cycles=0, cpu_state=cpu_state, _print=_print)
+            current_instr += 1  # we're done with this instruction!
+            if gen_test_str:
+                test_str += f"End of cycle {cycle_num:>2}: finish executing {str(instr_list[current_instr-1])}"
+                if instr_list[current_instr-1].name != Instruction.NOOP:
+                    test_str += f" (Register X is now {cpu_state['register_x']})"
+                test_str += '\n'
+                if prior_cpu_register_x_val != cpu_state['register_x']:
+                    test_str += f"Sprite position: {generate_sprite_pos_str(cpu_state['register_x'])}\n"
+        ## finish test_str
+        if gen_test_str and cycle_num_end - cycle_num > 0:
+            test_str += '\n'
+            
+
+    return crt_str, test_str if gen_test_str else None
+
 if __name__ == '__main__':
     instr_list = parse_instruction_list_from_file('input.txt')
-    print(f'Part one: {calculate_signal_strength_sum(instr_list, _print=True)}')
-
+    # print(f'Part one: {calculate_signal_strength_sum(instr_list, _print=True)}')
+    # crt_str, test_str = generate_crt_printstr_parttwo(instr_list, cycle_num_end=21, gen_test_str=True, _print=True)
+    crt_str, _ = generate_crt_printstr_parttwo(instr_list)
+    print(crt_str)
 
 '''
 Potential switchups that might occur for part two:
@@ -99,6 +181,10 @@ Potential switchups that might occur for part two:
 - more than one CPU, instructions get mapped to different CPUs based on some rule
 
 Tried to parameterize the sim funciton to be able to address these if they come up!
+
+
+Actual switchup for part 2 -- simulating a CRT!  I did not expect that :P
+
 
 I was able to check my output using a simple spreadsheet:
 
@@ -112,4 +198,11 @@ noop             3                          2                               row 
 ...              ...                        ...
                 =IF(LEFT(A2, 4)="addx", B1+2, B1+1)
                                            =IF(LEFT(A2, 4)="addx", C1+RIGHT(A2, LEN(A2)-5), C1)
+
+
+I created unit tests based on the adventofcode problem description and worked examples.
+
+I matched the printout of their cycle-by-cycle execution for part two, and was able to directly compare printouts as the unittest.
+This process exposed the need for a "0-cycle" advance of the simulation, to allow for instructions
+(which occur BETWEEN cycles, on clock edges) to finish!
 '''
--- day10/test_solution.py
+++ day10/test_solution.py
@@ -25,6 +25,7 @@ class TestDay10(unittest.TestCase):
                 for instr, line in zip(instr_list, lines):
                     self.assertEqual(str(instr), line)
 
+    # part one
     def test_partone_register_snapshots(self):
         '''Day10 part one: simulating instructions on a CPU.
         At specific cycle counts, need to sample the CPU's X register value.  Testing that here!'''
@@ -37,6 +38,157 @@ class TestDay10(unittest.TestCase):
     def test_partone_signal_strength_sum(self):
         instr_list = solution.parse_instruction_list_from_file(self.EXAMPLETWO)
         self.assertEqual(solution.calculate_signal_strength_sum(instr_list), 13140)
+
+    # part two
+    def test_sprite_pos_str_generation(self):
+        outcomes = [
+            # from adventofcode problem description
+            ( 1, '###.....................................'),
+            (16, '...............###......................'),
+            ( 5, '....###.................................'),
+            (11, '..........###...........................'),
+            ( 8, '.......###..............................'),
+            (13, '............###.........................'),
+            (12, '...........###..........................'),
+            ( 4, '...###..................................'),
+            (17, '................###.....................'),
+            (21, '....................###.................'),
+            (20, '...................###..................'),
+            # my own test cases (focusing on edge cases)
+            ( 0, '##......................................'),
+            (-1, '#.......................................'),
+            (-2, '........................................'),
+            (38, '.....................................###'),
+            (39, '......................................##'),
+            (40, '.......................................#'),
+            (41, '........................................'),
+        ]
+
+        for x_pos, sprite_str in outcomes:
+            self.assertEqual(sprite_str, solution.generate_sprite_pos_str(x_pos, linewidth=40))
+
+    def test_crt_cycle_by_cycle_explanation(self):
+        answer = '''Sprite position: ###.....................................
+
+Start cycle   1: begin executing addx 15
+During cycle  1: CRT draws pixel in position 0
+Current CRT row: #
+
+During cycle  2: CRT draws pixel in position 1
+Current CRT row: ##
+End of cycle  2: finish executing addx 15 (Register X is now 16)
+Sprite position: ...............###......................
+
+Start cycle   3: begin executing addx -11
+During cycle  3: CRT draws pixel in position 2
+Current CRT row: ##.
+
+During cycle  4: CRT draws pixel in position 3
+Current CRT row: ##..
+End of cycle  4: finish executing addx -11 (Register X is now 5)
+Sprite position: ....###.................................
+
+Start cycle   5: begin executing addx 6
+During cycle  5: CRT draws pixel in position 4
+Current CRT row: ##..#
+
+During cycle  6: CRT draws pixel in position 5
+Current CRT row: ##..##
+End of cycle  6: finish executing addx 6 (Register X is now 11)
+Sprite position: ..........###...........................
+
+Start cycle   7: begin executing addx -3
+During cycle  7: CRT draws pixel in position 6
+Current CRT row: ##..##.
+
+During cycle  8: CRT draws pixel in position 7
+Current CRT row: ##..##..
+End of cycle  8: finish executing addx -3 (Register X is now 8)
+Sprite position: .......###..............................
+
+Start cycle   9: begin executing addx 5
+During cycle  9: CRT draws pixel in position 8
+Current CRT row: ##..##..#
+
+During cycle 10: CRT draws pixel in position 9
+Current CRT row: ##..##..##
+End of cycle 10: finish executing addx 5 (Register X is now 13)
+Sprite position: ............###.........................
+
+Start cycle  11: begin executing addx -1
+During cycle 11: CRT draws pixel in position 10
+Current CRT row: ##..##..##.
+
+During cycle 12: CRT draws pixel in position 11
+Current CRT row: ##..##..##..
+End of cycle 12: finish executing addx -1 (Register X is now 12)
+Sprite position: ...........###..........................
+
+Start cycle  13: begin executing addx -8
+During cycle 13: CRT draws pixel in position 12
+Current CRT row: ##..##..##..#
+
+During cycle 14: CRT draws pixel in position 13
+Current CRT row: ##..##..##..##
+End of cycle 14: finish executing addx -8 (Register X is now 4)
+Sprite position: ...###..................................
+
+Start cycle  15: begin executing addx 13
+During cycle 15: CRT draws pixel in position 14
+Current CRT row: ##..##..##..##.
+
+During cycle 16: CRT draws pixel in position 15
+Current CRT row: ##..##..##..##..
+End of cycle 16: finish executing addx 13 (Register X is now 17)
+Sprite position: ................###.....................
+
+Start cycle  17: begin executing addx 4
+During cycle 17: CRT draws pixel in position 16
+Current CRT row: ##..##..##..##..#
+
+During cycle 18: CRT draws pixel in position 17
+Current CRT row: ##..##..##..##..##
+End of cycle 18: finish executing addx 4 (Register X is now 21)
+Sprite position: ....................###.................
+
+Start cycle  19: begin executing noop
+During cycle 19: CRT draws pixel in position 18
+Current CRT row: ##..##..##..##..##.
+End of cycle 19: finish executing noop
+
+Start cycle  20: begin executing addx -1
+During cycle 20: CRT draws pixel in position 19
+Current CRT row: ##..##..##..##..##..
+
+During cycle 21: CRT draws pixel in position 20
+Current CRT row: ##..##..##..##..##..#
+End of cycle 21: finish executing addx -1 (Register X is now 20)
+Sprite position: ...................###..................
+'''
+        instr_list = solution.parse_instruction_list_from_file(self.EXAMPLETWO)
+        _, test_str = solution.generate_crt_printstr_parttwo(instr_list, cycle_num_end=21, gen_test_str=True)
+        
+        old_maxdiff = self.maxDiff
+        self.maxDiff = None
+
+        self.assertEqual(answer, test_str)
+
+        self.maxDiff = old_maxdiff
+
+    def test_crt_printout(self):
+        answer = '''##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....'''
+        instr_list = solution.parse_instruction_list_from_file(self.EXAMPLETWO)
+        crt_str, _ = solution.generate_crt_printstr_parttwo(instr_list)
+        
+        old_maxdiff = self.maxDiff
+        self.maxDiff = None
+        self.assertEqual(answer, crt_str)
+        self.maxDiff = old_maxdiff
 
 if __name__ == '__main__':
     unittest.main()
