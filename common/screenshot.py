3b9f775f8a1a6a167aa96425e760136bd9157d35
lyubogankov <lyubogankov13@gmail.com>
2023-02-19 11:35:29 -0800

    moved save_folder from screenshot() argument -> ScreenshotParams field

--- common/screenshot.py
+++ common/screenshot.py
@@ -5,11 +5,11 @@ from collections import namedtuple
 import mss
 import pyautogui
 
-ScreenshotParams = namedtuple('ScreenshotParams', ['topoffset', 'leftoffset', 'width', 'height', 'monitor', 'framefolder'])
+ScreenshotParams = namedtuple('ScreenshotParams', ['topoffset', 'leftoffset', 'width', 'height', 'monitor', 'savefolder', 'framefolder'])
 
-def screenshot(screenshotter, screenshotparams, sim_iteration, savefolder=''):
+def screenshot(screenshotter, screenshotparams, sim_iteration):
     # with mss.mss() as screenshotter:
-    mon_info = screenshotter.monitors[screenshotparams.monitor]  # my second monitor
+    mon_info = screenshotter.monitors[screenshotparams.monitor]
     frame = screenshotter.grab({
         'top'    : mon_info['top']  + screenshotparams.topoffset, 
         'left'   : mon_info['left'] + screenshotparams.leftoffset, 
@@ -17,7 +17,8 @@ def screenshot(screenshotter, screenshotparams, sim_iteration, savefolder=''):
         'height' : screenshotparams.height,
         'mon'    : screenshotparams.monitor
     })
-    mss.tools.to_png(frame.rgb, frame.size, output=f'/home/lyubo/script/advent_of_code/2022/media/{savefolder}/{screenshotparams.framefolder}/frame_{sim_iteration}.png')
+    saveimg = f'/home/lyubo/script/advent_of_code/2022/media/{screenshotparams.savefolder}/{screenshotparams.framefolder}/frame_{sim_iteration}.png'
+    mss.tools.to_png(frame.rgb, frame.size, output=saveimg)
     time.sleep(0.1)
     # move the mouse to keep the computer from going to sleep
     pyautogui.move(0, -1)
