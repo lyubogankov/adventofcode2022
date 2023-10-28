f601b68aa8ac36eddd5a43f7f7ca9c58e1c96034
lyubogankov <lyubogankov13@gmail.com>
2023-10-27 19:42:01 -0700

    working on incorporating simulation into the animation event loop

--- day14/gui_visualizer.py
+++ day14/gui_visualizer.py
@@ -3,33 +3,33 @@ import pygame
 import solution
 from solution import Board, BoundingBox, Point
 
-def animate_frames(board: Board, viewbounds: BoundingBox = None, framerate: int=60):
+def animate_frames(board: Board, viewbounds: BoundingBox = None, framerate: int=10):
     """
     
     `viewbounds` overrides use of `board.rock_bounding_box`
     """
 
+    ### start the simulation
+    framegen = solution.run_simulation_frame_generator(board, create_board_frame_fn=lambda board, sand_unit: sand_unit)
+
     ### calculating screen size
     MAX_WIDTH = 1600
     MAX_HEIGHT = 900
 
-    if viewbounds:
-        boundingbox = BoundingBox(
-            topleft = viewbounds.topleft - Point(1, 1),
-            bottomright = viewbounds.bottomright + Point(2, 2)
-        )
-    else:
-        boundingbox = BoundingBox(
-            topleft = board.rock_bounding_box.topleft - Point(1, 1),
-            bottomright = board.rock_bounding_box.bottomright + Point(2, 2) 
-        )
+    bb = viewbounds if viewbounds else board.rock_bounding_box
+    boundingbox = BoundingBox(
+        topleft = bb.topleft - Point(1, 1),
+        bottomright = bb.bottomright + Point(2, 2)
+    )
     # want to preserve aspect ratio of board -- want tiles to be square.
     # TODO implement logic for this.  Example will be fine without.
     screen_width  = min(MAX_WIDTH,  boundingbox.width() *50)
     screen_height = min(MAX_HEIGHT, boundingbox.height()*50)
 
-    board_to_screen_x = lambda board_x: (board_x - boundingbox.topleft.x)*(screen_width  / boundingbox.width())
-    board_to_screen_y = lambda board_y: (board_y - boundingbox.topleft.y)*(screen_height / boundingbox.height())
+    def board_to_screen_x(board_x):
+        return (board_x - boundingbox.topleft.x)*(screen_width  / boundingbox.width())
+    def board_to_screen_y(board_y):
+        return (board_y - boundingbox.topleft.y)*(screen_height / boundingbox.height())
 
     screen_tile_px = board_to_screen_x(boundingbox.bottomright.x) / boundingbox.width()
 
@@ -61,10 +61,18 @@ def animate_frames(board: Board, viewbounds: BoundingBox = None, framerate: int=
                 surface=screen, color='gray', 
                 rect=(board_to_screen_x(rock.x), board_to_screen_y(rock.y), screen_tile_px, screen_tile_px)
             )
-            # print(rock.x, board_to_screen_x(rock.x))
-            # print(rock.y, board_to_screen_y(rock.y))
-            # print(screen_tile_px)
-            # breakpoint()
+        for sand in board.settled_sand:
+            pygame.draw.rect(
+                surface=screen, color='gold3',
+                rect=(board_to_screen_x(sand.x), board_to_screen_y(sand.y), screen_tile_px, screen_tile_px)
+            )
+
+        # next simulation frame
+        # fallingsand = next(framegen)
+        # pygame.draw.rect(
+        #     surface=screen, color='gold',
+        #     rect=(board_to_screen_x(fallingsand.x), board_to_screen_y(fallingsand.y), screen_tile_px, screen_tile_px)
+        # )
 
         pygame.display.flip()
         dt = clock.tick(framerate) / 1000
