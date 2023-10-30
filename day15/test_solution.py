6b5b74390967d6de953d7fe035c8e760cd1bc3de
lyubogankov <lyubogankov13@gmail.com>
2023-10-30 13:54:09 -0700

    created test for exclusion_zone_printout

--- day15/test_solution.py
+++ day15/test_solution.py
@@ -2,6 +2,7 @@
 import unittest
 # local
 import solution
+from solution import CoordPair
 import string_visualizer
 
 class TestDay15(unittest.TestCase):
@@ -13,7 +14,13 @@ class TestDay15(unittest.TestCase):
         parsed_map = string_visualizer.visualize_sensor_beacon_map(inputfile='example.txt')
         self.assertEqual(expected, parsed_map)
 
-    
+    def test_exclusion_zone_calculation(self):
+        expected = """"""
+        exclusion_printout = string_visualizer.visualize_sensor_beacon_map(
+            inputfile='example.txt',
+            show_excl_sensor_coords=[CoordPair(8, 7)]
+        )
+        self.assertEqual(expected, exclusion_printout)
 
 if __name__ == '__main__':
     unittest.main()
