c5081525094c586f0cd7745803a3f788f2856839
lyubogankov <lyubogankov13@gmail.com>
2023-11-18 18:14:52 -0800

    initializing screenshotter on module import, within module
    screenshot filenames have leading zeros and fixed width (5 numbers, works through 99999)

--- common/screenshot.py
+++ common/screenshot.py
@@ -7,7 +7,9 @@ import pyautogui
 
 ScreenshotParams = namedtuple('ScreenshotParams', ['topoffset', 'leftoffset', 'width', 'height', 'monitor', 'savefolder', 'framefolder'])
 
-def screenshot(screenshotter, screenshotparams, sim_iteration):
+_screenshotter = mss.mss()  # just make one
+
+def screenshot(screenshotparams, sim_iteration, screenshotter=_screenshotter):
     # with mss.mss() as screenshotter:
     mon_info = screenshotter.monitors[screenshotparams.monitor]
     frame = screenshotter.grab({
@@ -17,7 +19,7 @@ def screenshot(screenshotter, screenshotparams, sim_iteration):
         'height' : screenshotparams.height,
         'mon'    : screenshotparams.monitor
     })
-    saveimg = f'/home/lyubo/script/advent_of_code/2022/media/{screenshotparams.savefolder}/{screenshotparams.framefolder}/frame_{sim_iteration:<5}.png'
+    saveimg = f'/home/lyubo/script/advent_of_code/2022/media/{screenshotparams.savefolder}/{screenshotparams.framefolder}/frame_{sim_iteration:05}.png'
     mss.tools.to_png(frame.rgb, frame.size, output=saveimg)
     time.sleep(0.1)
     # move the mouse to keep the computer from going to sleep
